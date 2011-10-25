c5732bb1226220076ee902edc6bcedffa13e309a
Chris Anderson <jchris@apache.org>
2011-10-25 14:59:57 -0700

    initial list function to do lean JSON output and option for format=raw

---
+++ lists/format.js
@@ -0,0 +1,33 @@
+function(head, req) {
+    function sendJSON(obj, first) {
+        var seperator = first ? "" : ",\n";
+        send(seperator + JSON.stringify(obj, null, " "));
+    };
+    var format = req.query.format
+        , row
+        , first = true
+        ;
+    if (format == "raw") {
+        send('{"rows":[');
+        while (row = getRow()) {
+            sendJSON(row, first);
+            first = false;
+        }
+        send(']}');
+    } else if (format == "xml") {
+        // todo
+    } else { // default to "lean"
+        send('[');
+        while (row = getRow()) {
+            if (row.doc) {
+                delete row.doc._rev;
+                sendJSON(row.doc, first);
+            } else {
+                delete row.value;
+                sendJSON(row, first);
+            }
+            first = false;
+        }
+        send(']');
+    }
+};
